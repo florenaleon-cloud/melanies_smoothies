9372696ca4ad4253b8fdf1484a5d3ef6b6cc7cc6
florenaleon-cloud <f.lorenaleon@gmail.com>
2026-01-09 15:34:51 -0500

    Update streamlit_app.py
    update: apagado session=get_active_session()

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ st.write("The name on your Smoothie will be: ", name_on_order)
 
 #st.write("You fvorite fruit is:", option)
 
-session = get_active_session()
+#session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
