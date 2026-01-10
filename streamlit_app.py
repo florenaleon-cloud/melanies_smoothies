dbacdf08e1fec12ba9dce2c444723a8180e218cc
florenaleon-cloud <f.lorenaleon@gmail.com>
2026-01-09 19:14:18 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,27 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 
-# Snowflake connection (Streamlit Cloud)
+# Snowflake connection
 cnx = st.connection("snowflake")
-session = cnx.session()
+session = cnx.session()   # ← ESTO ERA EL ERROR
 
 st.title("🥤 Example Streamlit App")
 st.write("Choose the fruits you want in your custom Smoothie!")
 
 name_on_order = st.text_input("Name on Smoothie:")
 
-fruit_df = session.sql(
-    "SELECT FRUIT_NAME FROM SMOOTHIES.PUBLIC.FRUIT_OPTIONS"
-).to_pandas()
+fruit_df = (
+    session
+    .table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS")
+    .select(col("FRUIT_NAME"))
+    .collect()
+)
+
+fruit_options = [row.FRUIT_NAME for row in fruit_df]
 
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:",
-    fruit_df["FRUIT_NAME"].tolist(),
+    fruit_options,
     max_selections=5
 )
 
