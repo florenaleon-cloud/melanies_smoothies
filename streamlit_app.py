14a4c665f7cfd722e619b6126ded10468a3de711
florenaleon-cloud <f.lorenaleon@gmail.com>
2026-01-11 10:57:41 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ if ingredients_list:
         st.subheader(fruit_chosen+ ' Nutrition Information')
 
         smoothiefroot_response = requests.get(
-            f"https://my.smoothiefroot.com/api/fruit/{fruit_chosen.lower()}"
+            f"https://my.smoothiefroot.com/api/fruit/{search_on}"
         )
 
         sf_df = st.dataframe(
