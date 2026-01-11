49075629e1bc87889dc063cc9dd01787a013a284
florenaleon-cloud <f.lorenaleon@gmail.com>
2026-01-11 09:00:03 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,5 +44,5 @@ if time_to_insert:
         st.error("Please enter your name and select at least one ingredient!")
 
 import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon") 
-st.text(smoothiefroot_response.json())
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon", timeout=10) 
+st.text(smoothiefroot_response)
